cf96464cab4c82cf39c8868bf461ff357d6fbedf
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-10-04 19:10:30 +0100

    done

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -3,6 +3,9 @@ import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
+
+
+
 import { ThemedView } from '@/components/ThemedView';
 import { Colors } from '@/constants/Colors';
 
